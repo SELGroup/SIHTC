a577ab6968bf139b09c0912a97cbfbc8f7bdb7ed
qtliu <103512628+qtliu@users.noreply.github.com>
2025-04-18 23:31:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -29,9 +29,7 @@ def parse():
     parser.add_argument('--data', type=str, default='WebOfScience')
     parser.add_argument('--batch', type=int, default=32)
     parser.add_argument('--epoch', type=int, default=30)
-    # parser.add_argument('--early-stop', type=int, default=6)
     parser.add_argument('--device', type=str, default='cuda')
-    # parser.add_argument('--name', type=str, required=True)
     parser.add_argument('--name', type=str, default='test')
     parser.add_argument('--update', type=int, default=1)
     parser.add_argument('--model', type=str, default='prompt')
@@ -44,8 +42,7 @@ def parse():
     parser.add_argument('--label-loss-wight', type=float, default=0.0005)
     parser.add_argument('--hie-label-loss-wight', type=float, default=0.0005)
     parser.add_argument('--seloss-wight', type=float, default=0.05)
-    # parser.add_argument('--warmup-steps', type=int, default=1000)  # 新增
-    # parser.add_argument('--total-steps', type=int, default=10000)  # 新增
+
     return parser
 
 
@@ -63,25 +60,8 @@ class Save:
                     'score': score, 'args': self.args,
                     'best_score': best_score},
                    name)
-        
-# def get_scheduler(optimizer, warmup_steps, total_steps): # 线性衰减
-#     def lr_lambda(current_step: int):
-#         if current_step < warmup_steps:
-#             return float(current_step) / float(max(1, warmup_steps))
-#         return max(0.0, float(total_steps - current_step) / float(max(1, total_steps - warmup_steps)))
-#     return LambdaLR(optimizer, lr_lambda)
 
-# def get_cosine_with_warmup_scheduler(optimizer, warmup_steps, total_steps): # 余弦退火
-#     def lr_lambda(current_step: int):
-#         if current_step < warmup_steps:
-#             return float(current_step) / float(max(1, warmup_steps))
-#         else:
-#             # 余弦退火部分
-#             progress = float(current_step - warmup_steps) / float(max(1, total_steps - warmup_steps))
-#             return 0.5 * (1 + torch.cos(progress * 3.141592653589793))
-#     return LambdaLR(optimizer, lr_lambda)
-
-def get_exponential_with_warmup_scheduler(optimizer, warmup_steps, total_steps, gamma): # 指数衰减
+def get_exponential_with_warmup_scheduler(optimizer, warmup_steps, total_steps, gamma):
     def lr_lambda(current_step: int):
         if current_step < warmup_steps:
             return float(current_step) / float(max(1, warmup_steps))
@@ -94,11 +74,7 @@ if __name__ == '__main__':
     parser = parse()
     args = parser.parse_args()
     if args.wandb:
-        # wandb.init(config=args, project=str(args.batch) +'_'+ args.data + '_' + 'SIDHTC', name=args.name)
         wandb.init(project=str(args.batch) +'_'+ args.data + '_' + 'SISHTC', name=args.name)
-        # args.seloss_wight = wandb.config.seloss_wight
-        # args.label_loss_wight = wandb.config.label_loss_wight
-        # args.hie_label_loss_wight = wandb.config.hie_label_loss_wight
     print(args)
     utils.seed_torch(args.seed)
 
@@ -133,13 +109,13 @@ if __name__ == '__main__':
         return depth
 
 
-    depth_dict = {i: get_depth(i) for i in range(num_class)} # 每个标签所在的深度
+    depth_dict = {i: get_depth(i) for i in range(num_class)} 
     max_depth = depth_dict[max(depth_dict, key=depth_dict.get)] + 1
     depth2label = {i: [a for a in depth_dict if depth_dict[a] == i] for i in range(max_depth)}
 
     for depth in depth2label:
         for l in depth2label[depth]:
-            path_list.append((num_class + depth, l)) # 添加虚拟节点的连边
+            path_list.append((num_class + depth, l)) 
 
     if args.model == 'prompt':
         if os.path.exists(os.path.join(data_path, args.model)):
@@ -150,29 +126,29 @@ if __name__ == '__main__':
                                                         'dev': 'data/{}/{}_dev.json'.format(args.data, args.data),
                                                         'test': 'data/{}/{}_test.json'.format(args.data, args.data), })
 
-            prefix = [] # prompt的构建 # tokenizer.vocab_size=30522 num_class=141 max_depth=2
+            prefix = [] 
             for i in range(max_depth):
                 prefix.append(tokenizer.vocab_size + num_class + i)
                 prefix.append(tokenizer.vocab_size + num_class + max_depth)
             prefix.append(tokenizer.sep_token_id) # prefix = [30663, 30665, 30664, 30665, 102]
 
 
-            def data_map_function(batch, tokenizer): # batch中有token是原句，label是标签的索引
+            def data_map_function(batch, tokenizer): 
                 new_batch = {'input_ids': [], 'token_type_ids': [], 'attention_mask': [], 'labels': []}
                 for l, t in zip(batch['label'], batch['token']):
-                    new_batch['labels'].append([[-100 for _ in range(num_class)] for _ in range(max_depth)]) # new_batch['labels']=[[[第一层141],[第二层141]]]
-                    for d in range(max_depth): # 每一层
-                        for i in depth2label[d]: # 该层的每个标签
+                    new_batch['labels'].append([[-100 for _ in range(num_class)] for _ in range(max_depth)]) 
+                    for d in range(max_depth): 
+                        for i in depth2label[d]: 
                             new_batch['labels'][-1][d][i] = 0
                         for i in l:
                             if new_batch['labels'][-1][d][i] == 0:
                                 new_batch['labels'][-1][d][i] = 1
-                    new_batch['labels'][-1] = [x for y in new_batch['labels'][-1] for x in y] # 282维标签向量 将 new_batch['labels'] 中的最后一个元素从一个二维列表（列表的列表）转换为一维列表（扁平化的列表）
+                    new_batch['labels'][-1] = [x for y in new_batch['labels'][-1] for x in y] 
 
                     tokens = tokenizer(t, truncation=True)
-                    new_batch['input_ids'].append(tokens['input_ids'][:-1][:512 - len(prefix)] + prefix) # new_batch['input_ids']添加上模板变为一句话，中间没有102，结尾有102
+                    new_batch['input_ids'].append(tokens['input_ids'][:-1][:512 - len(prefix)] + prefix) 
                     new_batch['input_ids'][-1].extend(
-                        [tokenizer.pad_token_id] * (512 - len(new_batch['input_ids'][-1]))) # 每个句子添0补充为512维
+                        [tokenizer.pad_token_id] * (512 - len(new_batch['input_ids'][-1]))) 
                     new_batch['attention_mask'].append(
                         tokens['attention_mask'][:-1][:512 - len(prefix)] + [1] * len(prefix))
                     new_batch['attention_mask'][-1].extend([0] * (512 - len(new_batch['attention_mask'][-1])))
@@ -200,19 +176,17 @@ if __name__ == '__main__':
             json.dump(index, open(os.path.join(data_path, 'low.json'), 'w'))
         dataset['train'] = dataset['train'].select(index[len(index) // 5:len(index) // 10 * 3])
     model = Prompt.from_pretrained(args.arch, num_labels=len(label_dict), path_list=path_list, layer=args.layer,
-                                   graph_type=args.graph, data_path=data_path, depth2label=depth2label, seloss_wight=args.seloss_wight) # from_pretrained是什么意思？怎么用的？
+                                   graph_type=args.graph, data_path=data_path, depth2label=depth2label, seloss_wight=args.seloss_wight) 
     model.init_embedding()
 
     model.to('cuda')
     if args.wandb:
-        wandb.watch(model) # 假设在命令行中设置了 args.wandb = True，则该代码段将确保在训练过程中，模型的状态和性能指标会被记录和可视化到 wandb 的仪表板中，帮助用户实时了解模型的训练情况和性能表现
+        wandb.watch(model) 
 
     train = DataLoader(dataset['train'], batch_size=batch_size, shuffle=True, )
     dev = DataLoader(dataset['dev'], batch_size=8, shuffle=False)
     model.to('cuda')
     optimizer = Adam(model.parameters(), lr=args.lr)
-    # scheduler = get_scheduler(optimizer, args.warmup_steps, args.total_steps)  # 新增
-    # scheduler = get_cosine_with_warmup_scheduler(optimizer, warmup_steps=args.warmup_steps, total_steps=args.total_steps)
     lr_initial = args.lr
     lr_final = 1e-7
     gamma49 = (lr_final / lr_initial) ** (1 / 49)
@@ -239,7 +213,6 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join('checkpoints', args.name)):
         os.mkdir(os.path.join('checkpoints', args.name))
     
-    # 计算参数量
     def numParams(net):
         num = 0
         for param in net.parameters():
@@ -249,36 +222,21 @@ if __name__ == '__main__':
     print("numParams:", numParams(model))
 
     for epoch in range(args.epoch):
-
-        # 用于记录每个 epoch 的 loss 值
-        # loss_values = []
-
-        # if early_stop_count >= args.early_stop:
-        #     print("Early stop!")
-        #     with open('0.0005gl_lrexp_epoch50_agg_32_5_nytseloss.txt', 'a') as f:
-        #         f.write(f"Early stop!\n")
-        #     # break
         start_time = time.time()
         print("start_time:", start_time)
         model.train()
         with tqdm(train) as p_bar:
             for batch in p_bar: # batch包含'input_ids''attention_mask''labels'
                 batch = {k: v.to('cuda') if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
-                output, seloss, masked_lm_loss, multiclass_loss = model(**batch) # (**batch)：这是一个 Python 的解包操作符，用于将字典解包为关键字参数。换句话说，如果 batch 字典中有 {'input_ids': tensor1, 'attention_mask': tensor2, ...}，它会相当于 model(input_ids=tensor1, attention_mask=tensor2, ...)。
+                output, seloss, masked_lm_loss, multiclass_loss = model(**batch) 
 
-                # 获取self.weight()的值
-                graph_embedding_layer = model.get_input_embeddings()  # 获取GraphEmbedding对象
+                graph_embedding_layer = model.get_input_embeddings()  
                 label_weight_function = graph_embedding_layer.weight()[-num_class - len(depth2label) - 1: -len(depth2label)-1]
 
                 global_label_loss = global_singular_smoothing_loss(label_weight_function)
                 local_label_loss = local_singular_smoothing_loss(label_weight_function, depth2label, num_class)
 
                 loss_total = args.label_loss_wight * global_label_loss + args.hie_label_loss_wight * local_label_loss + output['loss']
-                
-                # # 记录 loss 值
-                # loss_values.append(seloss.item())
-                
-                # loss_total = output['loss']
                 loss_total.backward()
                 loss += loss_total.item()
                 update_step += 1
@@ -289,23 +247,16 @@ if __name__ == '__main__':
                     p_bar.set_description(
                         'loss:{:.4f}'.format(loss, ))
                     optimizer.step()
-                    scheduler.step()  # 新增
+                    scheduler.step()  
                     optimizer.zero_grad()
                     loss_total = 0
                     loss = 0
                     update_step = 0
-                    # torch.cuda.empty_cache()
+
         end_time = time.time()
         print("end_time:", end_time)
         elapsed_train_time = end_time - start_time
         print(f"epoch train time:{elapsed_train_time}s")
-        # # 保存 loss 值到文件中
-        # with open('0.0005gl_lrexp_epoch50_agg_32_5_nytseloss.txt', 'a') as f:
-        #     # for loss in loss_values:
-        #     #     f.write(f"{loss} ")
-        #     # f.write(f"\n")
-        #     average = sum(loss_values) / len(loss_values)
-        #     f.write(f"平均值为: {average}\n")
 
         model.eval()
         pred = []
@@ -328,33 +279,18 @@ if __name__ == '__main__':
             print("end_time:", end_time)
             elapsed_eval_time = end_time - start_time
             print(f"epoch eval time:{elapsed_eval_time}s")
-            # 获取self.weight()的值
-            graph_embedding_layer = model.get_input_embeddings()  # 获取GraphEmbedding对象
-            label_weight_function = graph_embedding_layer.weight()[-num_class - len(depth2label) - 1:-1]
-            # label_d_loss = -torch.norm(label_weight_function, p='nuc') / torch.sum(
-            #     torch.norm(label_weight_function, dim=1))
-            # print('label singular loss: ', label_d_loss)
-            _, label_s, _ = torch.svd(label_weight_function)
-            label_s = label_s.cpu().data.numpy()
-            label_s = label_s / np.max(label_s)
+
             if args.wandb:
                 wandb.log({'label_ausc': np.sum(label_s)})
-            # print(label_s.tolist())
-
-            # # 输出嵌入矩阵的值
-            # print(label_weight_function)
 
         scores = evaluate(pred, gold, label_dict)
         macro_f1 = scores['macro_f1']
         micro_f1 = scores['micro_f1']
         print('macro', macro_f1, 'micro', micro_f1)
 
-        # with open('0.0005gl_lrexp_epoch50_agg_32_5_nytseloss.txt', 'a') as f:
-        #     f.write(f"macro: {macro_f1}, micro:{micro_f1}\n")
-
         if args.wandb:
             wandb.log({'val_macro': macro_f1, 'val_micro': micro_f1})
-        # early_stop_count += 1
+
         if macro_f1 > best_score_macro:
             best_score_macro = macro_f1
             save(macro_f1, best_score_macro, os.path.join('checkpoints', args.name, 'checkpoint_best_macro.pt'))
@@ -363,8 +299,7 @@ if __name__ == '__main__':
         if micro_f1 > best_score_micro:
             best_score_micro = micro_f1
             save(micro_f1, best_score_micro, os.path.join('checkpoints', args.name, 'checkpoint_best_micro.pt'))
-            # early_stop_count = 0
-        # save(macro_f1, best_score, os.path.join('checkpoints', args.name, 'checkpoint_{:d}.pt'.format(epoch)))
+
         save(micro_f1, best_score_micro, os.path.join('checkpoints', args.name, 'checkpoint_last.pt'))
         if args.wandb:
             wandb.log({'best_macro': best_score_macro, 'best_micro': best_score_micro})
@@ -395,30 +330,12 @@ if __name__ == '__main__':
                             if l == 1:
                                 gold[-1].append(i)
 
-            # 获取self.weight()的值
-            graph_embedding_layer = model.get_input_embeddings()  # 获取GraphEmbedding对象
-            label_weight_function = graph_embedding_layer.weight()[-num_class - len(depth2label) - 1:-1]
-            label_d_loss = -torch.norm(label_weight_function, p='nuc') / torch.sum(
-                torch.norm(label_weight_function, dim=1))
-            print('label singular loss: ', label_d_loss)
-            _, label_s, _ = torch.svd(label_weight_function)
-            label_s = label_s.cpu().data.numpy()
-            label_s = label_s / np.max(label_s)
-            print('label singular values trend seed3 loss: ', np.sum(label_s))
-            print(label_s.tolist())
-
-            # 输出嵌入矩阵的值
-            print(label_weight_function)
-
         scores = evaluate(pred, gold, label_dict)
         macro_f1 = scores['macro_f1']
         micro_f1 = scores['micro_f1']
         print('macro', macro_f1, 'micro', micro_f1)
         print("---------------------")
         print(scores['full'])
-
-        # with open('0.0005gl_lrexp_epoch50_agg_32_5_nytseloss.txt', 'a') as f:
-        #     f.write(f"TEST macro: {macro_f1}, micro:{micro_f1}\n")
 
         with open(os.path.join('checkpoints', args.name, 'result{}.txt'.format(extra)), 'w') as f:
             print('macro', macro_f1, 'micro', micro_f1, file=f)
@@ -431,16 +348,3 @@ if __name__ == '__main__':
     test_function('_micro')
 
     wandb.finish()
-
-# if __name__ == '__main__':
-#     parser = parse()
-#     args = parser.parse_args()
-#     with open('sweep_config.yaml') as file:
-#         sweep_config = yaml.safe_load(file)
-#     # 创建 Sweep
-#     # sweep_id = wandb.sweep(sweep_config, project=str(args.batch) +'_'+ args.data + '_' + 'SIDHTC')
-#     # print("Sweep ID:", sweep_id)
-#     # 运行 sweep
-
-#     sweep_id = 'sia4vghk'
-#     wandb.agent(sweep_id, project='32_nyt_SIDHTC', function=train)
